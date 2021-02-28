ecbb574cf2de1ce26bd014b02af824f5433cb3d1
YXGuan <55643200+YXGuan@users.noreply.github.com>
2021-02-28 15:57:57 -0500

    Add project files.

---
+++ customer_new/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace customer_new.Models
+{
+    public class UserModel
+    {
+        public int Name { get; set; }
+        public int EmailAddress { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        //ctrl . auto generate constructor
+        public UserModel(int n, int e, int p)
+        {
+            Name = n;
+            EmailAddress = e;
+            PhoneNumber = p;
+        }
+    }
+}
